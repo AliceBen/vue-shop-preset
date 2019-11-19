390ad7acd9f9af708491262619329ac59fbfb071
Polaris <5470823+qinCute@user.noreply.gitee.com>
2019-11-19 20:07:37 +0800

    登录布局

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+<style lang="less" scoped>
+.login_container{
+  background: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top:50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入全局样式
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
