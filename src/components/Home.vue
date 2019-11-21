e26091b0ff224123efc678174664224707322ae8
AliceBen <angelQin0704@163.com>
2019-11-21 23:21:17 +0800

    退出登录

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>Home组件</div>
+    <div>Home组件
+        <el-button type="success" @click="logout">退出</el-button>
+    </div>
 </template>
 <script>
 export default {
-    
+    methods:{
+        logout(){
+            window.sessionStorage.clear();
+            this.$router.push("/login")
+        }
+    }
 }
 </script>
 <style lang="less" scoped>
