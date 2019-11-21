bc23073c4dd08d92185275673767bd4e00e5fc51
AliceBen <angelQin0704@163.com>
2019-11-21 23:16:43 +0800

    路由权限设置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -32,7 +32,6 @@
   </div>
 </template>
 <script>
-import { async } from 'q';
 export default {
   data() {
     return {
@@ -61,19 +60,21 @@ export default {
       this.$refs[formName].validate(async valid => {
         if (valid) {
           // await 只能用在被 async 修饰的方法中
-            const { data : res } = await this.$http.post("login",this.loginForm);
-            if(res.meta.status !== 200 ) {
-              return  this.$message.error('账号密码错误!');
-            }else{
-               this.$message({
-                message: '登录成功',
-                type: 'success'
+          const { data: res } = await this.$http.post("login", this.loginForm);
+          if (res.meta.status !== 200) {
+            return this.$message.error("账号密码错误!");
+          } else {
+            this.$message({
+              message: "登录成功",
+              type: "success"
             });
-          };
-          // 1.登录成功之后的 token，保存到客户端的sessionStorage中
-          //  1.1 项目中除了登录之外的API接口，必须在登录之后才能访问
-          //  1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
-          // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+            // 1.登录成功之后的 token，保存到客户端的sessionStorage中
+            //  1.1 项目中除了登录之外的API接口，必须在登录之后才能访问
+            //  1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+            window.sessionStorage.setItem("token", res.data.token);
+            // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+            this.$router.push("/home");
+          }
         }
       });
     },
